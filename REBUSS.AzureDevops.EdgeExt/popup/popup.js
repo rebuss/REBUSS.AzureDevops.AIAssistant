407011963663148b358fb01afd03acb8d38c34e6
STUDIO\rebuss <rebuss@users.noreply.github.com>
2025-01-30 13:18:58 +0100

    Removed EdgeExt folder as it will no longer be developed as open-source.

--- REBUSS.AzureDevops.EdgeExt/popup/popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("sendQuery").addEventListener("click", async () => {
-    const query = document.getElementById("query").value;
-    const response = await chrome.runtime.sendMessage({ action: "queryCopilot", query });
-    document.getElementById("response").textContent = JSON.stringify(response, null, 2);
-});
